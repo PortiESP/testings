d9b535274d17ca624093e95aeef05c02616c72fb
Porti <jaime.portilloperez@gmail.com>
2021-06-25 16:52:03 +0200

    updated

--- portiOSDriver_test.py
+++ portiOSDriver_test.py
@@ -3,6 +3,7 @@ import threading, Adafruit_ADS1x15, time
 
 class portiOSDriver:
 	def __init__(self, pins):
+		self.pins = pins
 		self.pin_Q0 = pins[0]
 		self.pin_Q1 = pins[1]
 		self.pin_Q2 = pins[2]
@@ -12,13 +13,13 @@ class portiOSDriver:
 		
 
 		gp.setmode(gp.BOARD)
-		map(lambda x: gp.setmode(x, gp.IN, pull_up_down=PUD_DOWN), pins)
+		map(lambda x: gp.setmode(x, gp.IN, pull_up_down=PUD_DOWN), self.pins)
 
 		self.a2d = Adafruit_ADS1x15.ADS1115()
 
 	def startListeners(self):
-		gp.add_event_detect(pin_EI, gp.RISING, callback=self.encoderCallback, bouncetime=300)
-		gp.add_event_detect(pin_CodBtn, gp.RISING, callback=self.rotEncoderButtonCallback, bouncetime=300)
+		gp.add_event_detect(self.pin_EI, gp.RISING, callback=self.encoderCallback, bouncetime=300)
+		gp.add_event_detect(self.pin_CodBtn, gp.RISING, callback=self.rotEncoderButtonCallback, bouncetime=300)
 
 
 	def encoderCallback(self):
@@ -30,7 +31,7 @@ class portiOSDriver:
 		elif data == 5:
 			print("IRR data")
 		elif data == 6:
-			print("Encoder rotation:", str(gp.input(pin_CodData)))
+			print("Encoder rotation:", str(gp.input(self.pin_CodData)))
 		elif data == 7:
 			print("Joystick button")
 
